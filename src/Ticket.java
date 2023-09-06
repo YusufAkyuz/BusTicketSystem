0ba35de786cb869ea67d3446b840d606db95c5cd
Yusuf Akyüz <yusufakyus47@gmail.com>
2023-09-06 11:29:36 +0300

    Ticket Class Written

---
+++ src/Ticket.java
@@ -0,0 +1,8 @@
+public class Ticket {
+    Person [] person;
+    private int price;
+    public Ticket(Person [] person, int price) {
+        this.person = person;
+        this.price = price;
+    }
+}
